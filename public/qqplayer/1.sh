0ad8e8af9282ebafc3c31afb9036de682cd8c7c9
qianjigui <qianjigui@gmail.com>
2018-10-30 21:31:48 +0800

    Add data

---
+++ public/qqplayer/1.sh
@@ -0,0 +1,13 @@
+set -x
+PATH='/Users/wpc/Downloads/AliGenieDemo 2018-10-30 21-19-42/Apps/AliGenieDemo.ipa'
+cp "$PATH" 'RLAudioRecord-iPad (4th generation)-etc.ipa'
+cp "$PATH" 'RLAudioRecord-iPad 2-etc.ipa'
+cp "$PATH" 'RLAudioRecord-iPad Air 2-etc.ipa'
+cp "$PATH" 'RLAudioRecord-iPad Pro (9.7-inch)-etc.ipa'
+cp "$PATH" 'RLAudioRecord-iPhone 5-etc.ipa'
+cp "$PATH" 'RLAudioRecord-iPhone 6s Plus-etc.ipa'
+cp "$PATH" 'RLAudioRecord-iPhone 7 Plus-etc.ipa'
+cp "$PATH" 'RLAudioRecord-iPhone 8-etc.ipa'
+cp "$PATH" 'RLAudioRecord-iPhone SE-etc.ipa'
+cp "$PATH" 'RLAudioRecord-iPod touch-etc.ipa'
+cp "$PATH" 'RLAudioRecord.ipa'
