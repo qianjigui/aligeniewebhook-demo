8532f3efb33f7ea9915ad1dae97ff79f896e6206
qianjigui <qianjigui@gmail.com>
2018-12-19 14:43:29 +0800

    Update data

--- public/qqplayer/1.sh
+++ public/qqplayer/1.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
 set -x
-PATH='/Users/wpc/Downloads/AliGenieDemo 2018-10-30 21-19-42/Apps/AliGenieDemo.ipa'
+PATH='/Users/wpc/Downloads/AliGenieDemo 2018-12-19 10-03-20/Apps/AliGenieDemo.ipa'
+ls "$PATH"
 cp "$PATH" 'RLAudioRecord-iPad (4th generation)-etc.ipa'
 cp "$PATH" 'RLAudioRecord-iPad 2-etc.ipa'
 cp "$PATH" 'RLAudioRecord-iPad Air 2-etc.ipa'
